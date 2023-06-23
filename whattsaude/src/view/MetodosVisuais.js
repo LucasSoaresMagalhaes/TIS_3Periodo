1c08defcff7417fd045d42944555264832c02f6f
Samuel carlos <samuelcarlos181@gmail.com>
2023-06-22 23:11:36 -0300

    Atualização banco de dados

--- whattsaude/src/view/MetodosVisuais.js
+++ whattsaude/src/view/MetodosVisuais.js
@@ -1,11 +1,23 @@
 function mostrarLayoutCriarConta() {
-
     document.getElementById("CriarConta").style.display = 'block';
     document.getElementById("Login").style.display = 'none';
-
 }
 
 function mostrarLayoutLogin() {
     document.getElementById("Login").style.display = 'block';
     document.getElementById("CriarConta").style.display = 'none';
 }
+
+function esconder(){
+    var numero = document.getElementById("inputPassword");
+    var botao = document.getElementById("button-addon1");
+    if(numero.type==="password"){
+        numero.type = "text"
+        botao.innerHTML = "Esconder"
+    }
+    else{
+        numero.type = "password"
+        botao.innerHTML = "Mostrar"
+    }
+}
+
--- whattsaude/src/view/README.md
+++ whattsaude/src/view/README.md
@@ -1,1 +1,10 @@
-ARQUIVOS DO FRONT DO PROJETO
+# TIS_3Periodo
+Repositório para o trabalho TIS terceiro período
+
+Futuras Implementações:
+
+    Sistema de agenda
+    
+    Sistema de dicas médicas (Lista de dicas que mostra uma diferente cada vez que a página recarrega)
+    
+    Sistema de cadastro de contatos por meio da pesquisa de pessoas cadastradas dentro do banco de dados
